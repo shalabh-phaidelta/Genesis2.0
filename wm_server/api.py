26e09d52f64a5fffbbdaaf1b3fef00dd6ec69db9
Ashpak-phaiDelta <a.shaikh@phaidelta.com>
2022-11-24 11:40:51 +0530

    cleared all commented code

--- wm_server/api.py
+++ wm_server/api.py
@@ -48,11 +48,6 @@ async def websocket_connect(websocket, msg):
 async def user_location(user: SystemUser = Depends(get_current_user)):
     return user_resources.UserLocation().get( user.user_id )
 
-#@app.websocket('/metrics/warehouse/{warehouse_id}')
-#async def websocket_endpoint( warehouse_id : int ,websocket: WebSocket , token: str = Query(...) ):
- #   user_info : SystemUser =  await get_user_info_from_token(token=token)
-  #  await websocket_connect(websocket, user_resources.Metrics_Warehouse().get(warehouse_id, user_info.user_id) )
-
 @app.websocket('/metrics/warehouse/{warehouse_id}')
 async def websocket_endpoint( warehouse_id : int ,websocket: WebSocket ):
     await websockets_manager.connect(websocket)
@@ -88,17 +83,9 @@ async def unit_details(warehouse_id : int,  user : SystemUser = Depends(get_curr
 async def unit_details(warehouse_id : int, unit_id : int, user : SystemUser = Depends(get_current_user)):
     return user_resources.Metrics_Unit().get(warehouse_id, unit_id, user.user_id)
 
-
 @app.get('/locations/{location_id}/summary', summary = 'Get details of current user location')
 async def less_qty(location_id : int, user : SystemUser = Depends(get_current_user)):
     return user_resources.Location_Summary().get(location_id, user.user_id)
-
-
-#@app.websocket('/metrics/warehouse/{warehouse_id}/unit/{unit_id}')
-#async def websocket_endpoint( warehouse_id : int, unit_id : int ,websocket: WebSocket , token: str = Query(...) ):
- #   user_info : SystemUser =  await get_user_info_from_token(token=token)
-  #  await websocket_connect(websocket, user_resources.Metrics_Unit().get(warehouse_id, unit_id,user_info.user_id) )
-
 
 @app.websocket('/metrics/warehouse/{warehouse_id}/unit/{unit_id}')
 async def websocket_endpoint( warehouse_id : int, unit_id : int ,websocket: WebSocket ):
@@ -126,7 +113,6 @@ async def websocket_endpoint( warehouse_id : int, unit_id : int ,websocket: WebS
             headers= {"WWW-Authenticate": "Bearer"}
         )
 
-
 @app.websocket("/websocket")
 async def websocket_endpoint(websocket: WebSocket):
     await websockets_manager.connect(websocket)
@@ -137,7 +123,6 @@ async def websocket_endpoint(websocket: WebSocket):
             await websocket.send_json( json.dumps({"hello":"world"}))
     except WebSocketDisconnect:
         await websockets_manager.disconnect(websocket)
-
 
 @app.post('/api/history/{metric_id}', summary = 'Get details of metric')
 async def metric_chart_history(metric_id : int, parameter : Report_Interval,  user : SystemUser = Depends(get_current_user)):
@@ -175,7 +160,6 @@ async def login(from_data : UserLogin):
         "refresh_token": create_refresh_token(from_data.username),
     }
 
-
 @app.post('/signup', summary = "Create new user")
 async def create_user(data: UserAuth):
     try:
@@ -209,7 +193,6 @@ async def websocket_endpoint(websocket: WebSocket , token: str = Query(...) ):
     user_info : SystemUser =  await get_user_info_from_token(token=token)
     await websocket_connect(websocket,  json.dumps({"hello" : "world"}))
     
-#    return user_resources.User().user_name_list()
 
 @app.get('/all_unit')
 async def get_all_unit():
@@ -239,64 +222,3 @@ async def get_all_user_location_mapping():
         user_location_mapping_info = session.query(db.db.User_location_unit_map).options(db.orm.Load(db.db.User_location_unit_map)).all()
         return user_location_mapping_info
 
-
-# ---------------------------------------------------------------------------------------
-@app.get('/check_websocket_wareshouse')
-async def check_websocket_wareshouse(user : SystemUser = Depends(get_current_user)):
-    client = TestClient(app)
-    warehouse_id = 1
-    with client.websocket_connect(f'/metrics/warehouse/{warehouse_id}?token={user.token}') as websocket:
-        data = json.loads(websocket.receive_json())
-        print(data)
-        # print('data recived from websocket:', data)
-    return data
-
-@app.get('/check_websocket')
-async def check_websocket(user : SystemUser = Depends(get_current_user)):
-    client = TestClient(app)
-    warehouse_id = 1
-    unit_id = 1
-    with client.websocket_connect(f'/metrics/warehouse/{warehouse_id}/unit/{unit_id}?token={user.token}') as websocket:
-        data = json.loads(websocket.receive_json())
-        print(data)
-        # print('data recived from websocket:', data)
-    return data
-       
-
-
-@app.post('/login_',summary = 'Create access and refresh tokens for user', response_model = TokenSchema)
-async def login(from_data : OAuth2PasswordRequestForm = Depends()):
-    try:
-        user = db.User().get_user_info_by_email(from_data.username)
-    except TypeError:
-        user = None
-    if user is None:
-        raise HTTPException(
-            status_code = status.HTTP_400_BAD_REQUEST,
-            detail = "Incorrect email or user_name"
-        )
-    hashed_password = user['password']
-    if not verify_password(from_data.password, hashed_password):
-        raise HTTPException(
-            status_code = status.HTTP_400_BAD_REQUEST,
-            detail = "Incorrect Email or password"
-        )
-    access_token = create_access_token(user['email'])
-    db.update_token_for_user(user['user_id'], access_token)
-    return {
-        "id_user" : user['user_id'],
-        'role' : 'CXO',
-        "token": access_token,
-        'username' : user['email'],
-        "refresh_token": create_refresh_token(user['email']),
-    }
-
-@app.get('/check_users')
-async def check_websocket_wareshouse(user : SystemUser = Depends(get_current_user)):
-    client = TestClient(app)
-    warehouse_id = 1
-    with client.websocket_connect(f'/users?token={user.token}') as websocket:
-        data = json.loads(websocket.receive_json())
-        print(data)
-        # print('data recived from websocket:', data)
-    return data
